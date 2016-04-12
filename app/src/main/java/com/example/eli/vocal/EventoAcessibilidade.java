00c9d45ce45d9c5737eb811188d0a3e6118d53e2
Eli <eli.t.desouza@gmail.com>
2016-04-11 23:35:12 -0300

    Desenv
    Dale TCC

--- app/src/main/java/com/example/eli/vocal/EventoAcessibilidade.java
+++ app/src/main/java/com/example/eli/vocal/EventoAcessibilidade.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -27,20 +28,21 @@ public class EventoAcessibilidade extends AccessibilityService {
     private final int ACAO_VOLTAR  = 6;
     private final int ACAO_BOTAO   = 7;
     private final int ACAO_ESCREVA = 8;
-    private final int ACAO_TESTE   = 9;
+    private final int ACAO_BUSCAR  = 9;
+    private final int ACAO_DISCAR  = 10;
 
     //Constantes de tipo de componentes de tela
-    private final String BOTAO         = "android.widget.Button";
-    private final String EDIT_TEXT     = "android.widget.EditText";
-    private final String SWITCH        = "android.widget.Switch";
-    private final String IMAGE_VIEW    = "android.widget.ImageView";
+    private final String BOTAO         = "Button";
+    private final String EDIT_TEXT     = "EditText";
+    private final String SWITCH        = "Switch";
+    private final String IMAGE_VIEW    = "ImageView";
+    private final String FRAME_LAYOUT  = "FrameLayout";
 
     private static EventoAcessibilidade eventoAcessibilidade;
 
     private String fraseRetorno;
     private TextToSpeech speaker;
     private HashMap<String,Integer> acoes;
-    //private AccessibilityNodeInfo nodeInfo;
 
     public static EventoAcessibilidade getInstance(){
         return eventoAcessibilidade;
@@ -62,80 +64,54 @@ public class EventoAcessibilidade extends AccessibilityService {
     private void inicializaAcoes(){
         acoes = new HashMap<>();
         //Acoes referentes a leitura
-        acoes.put("Leia",ACAO_LEITURA);
         acoes.put("leia",ACAO_LEITURA);
-        acoes.put("Ler",ACAO_LEITURA);
         acoes.put("ler",ACAO_LEITURA);
-        acoes.put("Le",ACAO_LEITURA);
         acoes.put("le",ACAO_LEITURA);
 
         //Acoes referentes a fala
-        acoes.put("Fala",ACAO_FALA);
         acoes.put("fala",ACAO_FALA);
-        acoes.put("Fale",ACAO_FALA);
         acoes.put("fale",ACAO_FALA);
-        acoes.put("Falar",ACAO_FALA);
         acoes.put("falar",ACAO_FALA);
-        acoes.put("Diga",ACAO_FALA);
         acoes.put("diga",ACAO_FALA);
 
         //Acoes referentes a desligar
-        acoes.put("Desliga",ACAO_DESLIGA);
         acoes.put("desliga",ACAO_DESLIGA);
-        acoes.put("Desligar",ACAO_DESLIGA);
         acoes.put("desligar",ACAO_DESLIGA);
-        acoes.put("Parar",ACAO_DESLIGA);
         acoes.put("parar",ACAO_DESLIGA);
 
         //Acoes referentes a tocar em um componente
-        acoes.put("Toque",ACAO_TOQUE);
         acoes.put("toque",ACAO_TOQUE);
-        acoes.put("Clique",ACAO_TOQUE);
         acoes.put("clique",ACAO_TOQUE);
-        acoes.put("Abra",ACAO_TOQUE);
         acoes.put("abra",ACAO_TOQUE);
-        acoes.put("Abre",ACAO_TOQUE);
         acoes.put("abre",ACAO_TOQUE);
 
         //Acoes para pressionar o botao HOME
-        acoes.put("Início",ACAO_HOME);
         acoes.put("início",ACAO_HOME);
-        acoes.put("Home",ACAO_HOME);
         acoes.put("home",ACAO_HOME);
-        acoes.put("Casa",ACAO_HOME);
         acoes.put("casa",ACAO_HOME);
 
         //Acoes para pressionar o botao Voltar
-        acoes.put("Voltar",ACAO_VOLTAR);
         acoes.put("voltar",ACAO_VOLTAR);
 
         //Acoes para pressionar botoes
-        acoes.put("Botão",ACAO_BOTAO);
         acoes.put("botão",ACAO_BOTAO);
 
         //Acoes para escrever em campos de texto
-        acoes.put("Escreva",ACAO_ESCREVA);
         acoes.put("escreva",ACAO_ESCREVA);
-        acoes.put("Escrever",ACAO_ESCREVA);
         acoes.put("escrever",ACAO_ESCREVA);
-        acoes.put("Escreve",ACAO_ESCREVA);
         acoes.put("escreve",ACAO_ESCREVA);
 
-        acoes.put("Teste",ACAO_TESTE);
-        acoes.put("teste",ACAO_TESTE);
+        acoes.put("buscar", ACAO_BUSCAR);
+
+        acoes.put("discar", ACAO_DISCAR);
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         final int tipoEvento = event.getEventType();
-        //AccessibilityNodeInfo nodeInfo;
         //System.out.println(event.eventTypeToString(event.getEventType()) + " Source:" + (event.getSource() == null));
-
         switch (tipoEvento){
             case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START:
-                //speaker.speak("Estou ouvindo", TextToSpeech.QUEUE_FLUSH, null);
-                //System.out.println("toString: " + event.toString());
-
                 //Iniciar intent para ouvir o usuario
                 Intent intent = new Intent();
                 intent.setClass(this, ReconhecedorDeVoz.class);
@@ -144,20 +120,9 @@ public class EventoAcessibilidade extends AccessibilityService {
 
                 break;
             case AccessibilityEvent.TYPE_VIEW_HOVER_ENTER:
-                //nodeInfo = event.getSource();
-                //System.out.println(event.getSource().toString());
                 break;
             case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
-                /*if (nodeInfo != null){
-                    System.out.println(event.getSource().getContentDescription());
-                    System.out.println(event.getSource().getChildCount());
-
-                    for (int i = 0; i < nodeInfo.getChildCount();i++){
-                        System.out.println(nodeInfo.getChild(i).toString());
-                    }
-                }*/
                 if (this.fraseRetorno != null && event.getSource() != null){
-                    //nodeInfo = event.getSource();
                     this.retornoReconhecimento(fraseRetorno, event.getSource());
                 }
                 break;
@@ -178,11 +143,9 @@ public class EventoAcessibilidade extends AccessibilityService {
     @Override
     public void onServiceConnected() {
         AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-        //info.flags = AccessibilityServiceInfo.DEFAULT;
         info.flags = AccessibilityServiceInfo.FLAG_RETRIEVE_INTERACTIVE_WINDOWS|AccessibilityServiceInfo.FLAG_REPORT_VIEW_IDS|AccessibilityServiceInfo.FLAG_REQUEST_TOUCH_EXPLORATION_MODE;
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
         info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
-        //info.eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED;
         info.notificationTimeout = 100;
 
         this.setServiceInfo(info);
@@ -200,7 +163,7 @@ public class EventoAcessibilidade extends AccessibilityService {
         int acao;
         try{
             //Encontrar o agrupador da palavra de acao
-            acao = acoes.get(frase[0]);
+            acao = acoes.get(frase[0].toLowerCase());
         } catch (Exception e){
             acao = 999;
         }
@@ -241,35 +204,17 @@ public class EventoAcessibilidade extends AccessibilityService {
                 }
                 break;
             case ACAO_HOME:
-                System.out.println("Passou home");
-                /*View view = new View(this);
-                view.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN,KeyEvent.KEYCODE_HOME));*/
-                /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-                    System.out.println("Passou home33");
-                    super.onKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_HOME));
-                    super.onKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN,KeyEvent.KEYCODE_HOME));
-                }*/
-
+                Intent startMain = new Intent(Intent.ACTION_MAIN);
+                startMain.addCategory(Intent.CATEGORY_HOME);
+                startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(startMain);
                 break;
             case ACAO_VOLTAR:
-                /*KeyEvent evento = new KeyEvent(KeyEvent.ACTION_DOWN,KeyEvent.KEYCODE_BACK);
-                evento.dispatch(evento);
-                System.out.println("Passou voltar222");*/
-                /*View view2 = new View(this);
-                Keyboard
-                View kbview = new KeyboardView(this,null);*/
-                /*System.out.println(view2.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN,KeyEvent.KEYCODE_BACK)));
-                System.out.println(view2.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_UP,KeyEvent.KEYCODE_BACK)));
-                view2.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_UP,KeyEvent.KEYCODE_BACK));
-                view2.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN,KeyEvent.KEYCODE_BACK));*/
-                System.out.println(nodeInfo.toString());
-                //nodeInfo.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT,new Bundle());
                 break;
             case ACAO_BOTAO:
-                executaAcao(nodeInfo, sujeito, BOTAO,AccessibilityNodeInfo.ACTION_CLICK, null);
+                executaAcao(nodeInfo, sujeito, BOTAO, AccessibilityNodeInfo.ACTION_CLICK, null);
                 break;
             case ACAO_ESCREVA:
-
                 //Montar frase a ser escrita pelo Vocal
                 String fraseEditText = "";
                 for (int i = 1;i < frase.length - 1;i++){
@@ -281,8 +226,12 @@ public class EventoAcessibilidade extends AccessibilityService {
                 clipboard.setPrimaryClip(clip);
                 executaAcao(nodeInfo, sujeito, EDIT_TEXT, AccessibilityNodeInfo.ACTION_PASTE, null);
                 break;
-            case ACAO_TESTE:
+            case ACAO_BUSCAR:
                 executaAcao(nodeInfo, sujeito, IMAGE_VIEW, AccessibilityNodeInfo.ACTION_CLICK, null);
+                break;
+            case ACAO_DISCAR:
+                System.out.println("passou aqui");
+                executaAcao(nodeInfo, sujeito, FRAME_LAYOUT, AccessibilityNodeInfo.ACTION_CLICK, null);
                 break;
             default:
                 speaker.speak("Desculpe, não entendi", TextToSpeech.QUEUE_FLUSH, null);
@@ -360,11 +309,11 @@ public class EventoAcessibilidade extends AccessibilityService {
 
         String className = nodeInfo.getClassName().toString();
 
-        if (className.equalsIgnoreCase(BOTAO)){
+        if (className.contains(BOTAO)){
             return "botão ";
-        } else if (className.equalsIgnoreCase(EDIT_TEXT)){
+        } else if (className.contains(EDIT_TEXT)){
             return "campo de texto ";
-        } else if (className.equalsIgnoreCase(SWITCH)){
+        } else if (className.contains(SWITCH)){
             return "chave ";
         }
         return "";
@@ -373,63 +322,94 @@ public class EventoAcessibilidade extends AccessibilityService {
     private void executaAcao(AccessibilityNodeInfo nodeInfo, String sujeito, String tipoComponente, int action, Bundle arguments){
         sujeito = converteNumeral(sujeito);
         System.out.println(sujeito);
-        List<AccessibilityNodeInfo> nos = nodeInfo.findAccessibilityNodeInfosByText(sujeito);
+        //List<AccessibilityNodeInfo> nos = nodeInfo.findAccessibilityNodeInfosByText(sujeito);
+        List<AccessibilityNodeInfo> nos = encontraNoTela(nodeInfo,sujeito, tipoComponente);
         System.out.println(sujeito);
         AccessibilityNodeInfo no = null;
 
         if (nos != null){
-            //System.out.println("tamanos nos: " + nos.size());
             for (int i = 0; i < nos.size();i++) {
                 System.out.println(nos.get(i).toString());
             }
             try{
-                if (tipoComponente != null){
+                /*if (tipoComponente != null){
+                    System.out.println("Cp: " + tipoComponente);
                     //Remover os nos que nao sao da classe desejada pela acao
                     for (int i = 0; i < nos.size();i++) {
-                        if (!tipoComponente.equalsIgnoreCase(nos.get(i).getClassName().toString())){
+                        System.out.println("Entrou no loop de filtro ");
+                        //if (!tipoComponente.equalsIgnoreCase(nos.get(i).getClassName().toString())){
+                        if (!(nos.get(i).getClassName().toString().contains(tipoComponente))){
                             nos.remove(i);
+                        } else {
+                            System.out.println("Aceitando: " + nos.get(i).getClassName().toString() + " Cp: " + tipoComponente);
                         }
                     }
-                }
+                }*/
+
+                System.out.println("Depois de filtrar-------");
 
                 for (int i = 0; i < nos.size();i++) {
                     System.out.println(nos.get(i).toString());
                 }
 
-                //Se a busca na tela encontrar apenas um no, ele sofrera a acao
-                //if (nos.size() == 1){
-                    no = nos.get(0);
-                /*} else {
-                    //Se a busca na tela encontrar mais de um no, eh preciso selecionar o no que tem o
-                    // texto exatamente igual ao sujeito
-                    for (int i = 0; i < nos.size();i++) {
-                        if (sujeito.equalsIgnoreCase(nos.get(i).getText().toString())){
-                            no = nos.get(i);
-                            i = nos.size();
-                        }
-                    }
-                }*/
+                no = nos.get(0);
+
                 if (no != null){
                     if (arguments == null){
                         no.performAction(action);
                     } else {
-                        //System.out.println(no.performAction(action, arguments));
-                        /*ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                        ClipData clip = ClipData.newPlainText("label", "Por favor xesus");
-                        clipboard.setPrimaryClip(clip);
-                        no.performAction(AccessibilityNodeInfo.ACTION_PASTE);*/
+                        no.performAction(action, arguments);
                     }
-                } else {
-                    System.out.println("no null");
                 }
             }catch (IndexOutOfBoundsException indexEx) {
                 speaker.speak("Não encontrei na tela " + sujeito, TextToSpeech.QUEUE_FLUSH, null);
+                System.out.println("Nao encontrou na tela");
+                System.out.println(nodeInfo.toString());
             } catch (Exception ex){
                 speaker.speak("Erro de tela desconhecido", TextToSpeech.QUEUE_FLUSH, null);
-                //System.out.println(ex.getMessage());
             }
         } else {
             System.out.println("Nos null");
         }
+    }
+
+    private List<AccessibilityNodeInfo> encontraNoTela(AccessibilityNodeInfo nodeInfo, String texto, String tipoComponente){
+        List<AccessibilityNodeInfo> nos = new ArrayList<>();
+        texto = texto.toLowerCase();
+        String textoNo = "";
+        if (nodeInfo == null){
+            return nos;
+        }
+
+        if (nodeInfo.getChildCount() > 0) {
+            for (int i = 0; i < nodeInfo.getChildCount(); i++) {
+                textoNo = "";
+                if (nodeInfo.getChild(i) != null){
+                    if (nodeInfo.getChild(i).getText() != null){
+                        textoNo += nodeInfo.getChild(i).getText().toString();
+                    }
+
+                    if (nodeInfo.getChild(i).getContentDescription() != null){
+                        textoNo += nodeInfo.getChild(i).getContentDescription().toString();
+                    }
+
+                    textoNo = textoNo.toLowerCase();
+
+                    if (textoNo.contains(texto)){
+                        if (tipoComponente == null){
+                            System.out.println("Add no: " + nodeInfo.getChild(i).toString());
+                            nos.add(nodeInfo.getChild(i));
+                        } else if (nodeInfo.getChild(i).getClassName().toString().contains(tipoComponente)){
+                            System.out.println("Add no: " + nodeInfo.getChild(i).toString());
+                            nos.add(nodeInfo.getChild(i));
+                        }
+                    }
+
+                    nos.addAll(encontraNoTela(nodeInfo.getChild(i),texto, tipoComponente));
+                }
+            }
+        }
+
+        return nos;
     }
 }
